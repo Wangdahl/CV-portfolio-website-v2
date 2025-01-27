dc9e36d806b6593d5d861bee1a10b69cb489f5cc
Melker Wångdahl <melker.wangdahl@gmail.com>
2025-01-27 10:30:00 +0100

    Front page CSS completed.

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+/* PDF download button */
+document.getElementById('cv-button').addEventListener('click', function() {
+    const pdfPath = '../resources/cv-melker-wangdahl.pdf';
+    const a = document.createElement('a');
+    a.href = pdfPath;
+    a.download = 'cv-melker-wangdahl.pdf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+})
